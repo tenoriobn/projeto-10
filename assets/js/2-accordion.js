4b8874710653d65822a35f563ed848861930f9ff
Bruno Tenorio <tenoriobn@gmail.com>
2023-03-12 21:09:35 -0300

    Corrigindo accordion

--- assets/js/2-accordion.js
+++ assets/js/2-accordion.js
@@ -1,14 +1,31 @@
-const accordionItem = document.querySelectorAll(".accordion");
+const accordionItem = document.querySelectorAll('.accordion');
 
 accordionItem.forEach(accordion => {
+    const accordionTexto = accordion.querySelector('.accordion__texto');
+
     accordion.addEventListener('click', () => {
-        removeClass(accordionItem)
-        accordion.classList.toggle('active')
+        removeClass(accordionItem);
+        removeActiveAccordionTexto()
+        accordion.classList.toggle('active');
+
+        if(accordion.classList.contains('active')) {
+            accordionTexto.classList.add('active');
+        }
     })
 })
 
 function removeClass(accordionItem) {
     accordionItem.forEach(accordion => {
-        accordion.classList.remove('active')
+        accordion.classList.remove('active');
     })
+}
+
+function removeActiveAccordionTexto() {
+    const accordionTexto = document.querySelectorAll('.accordion__texto');
+
+    accordionTexto.forEach(texto => {
+        if(!texto.closest('.accordion').classList.contains('active')) {
+            texto.classList.remove('active');
+        }
+    });
 }
